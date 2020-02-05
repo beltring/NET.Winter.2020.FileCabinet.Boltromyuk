73a814bdb02331566028bb30bec19290f4dc82ad
Pavel Boltromyuk <beltring99@gmail.com>
2020-02-05 18:45:11 +0300

    Add guard clauses to CreateRecord method.

--- FileCabinetApp/FileCabinetService.cs
+++ FileCabinetApp/FileCabinetService.cs
@@ -10,6 +10,8 @@ namespace FileCabinetApp
 
         public int CreateRecord(string firstName, string lastName, DateTime dateOfBirth, short salary, decimal workRate, char gender)
         {
+            Validation(firstName, lastName, dateOfBirth, salary, workRate, gender);
+
             var record = new FileCabinetRecord
             {
                 Id = this.list.Count + 1,
@@ -36,6 +38,50 @@ namespace FileCabinetApp
         public int GetStat()
         {
             return this.list.Count;
+        }
+
+        private static void Validation(string firstName, string lastName, DateTime dateOfBirth, short salary, decimal workRate, char gender)
+        {
+            if (firstName == null)
+            {
+                throw new ArgumentNullException($"The {nameof(firstName)} parameter must not be null.");
+            }
+
+            if (lastName == null)
+            {
+                throw new ArgumentNullException($"The {nameof(firstName)} parameter must not be null.");
+            }
+
+            if (firstName.Length < 2 || firstName.Length > 60)
+            {
+                throw new ArgumentException($"The length of {nameof(firstName)} must be between 2 and 60 characters.");
+            }
+
+            if (lastName.Length < 2 || lastName.Length > 60)
+            {
+                throw new ArgumentException($"The length of {nameof(lastName)} must be between 2 and 60 characters.");
+            }
+
+            if (dateOfBirth.Year < 1950 || (dateOfBirth.Year >= DateTime.Now.Year
+                && dateOfBirth.Month >= DateTime.Now.Month && dateOfBirth.Day > DateTime.Now.Day))
+            {
+                throw new ArgumentException("Minimum date of birth 01-Jan-1950 maximum current date.");
+            }
+
+            if (salary < 100 || salary > 10000)
+            {
+                throw new ArgumentException($"Invalid value, the {nameof(salary)} value must be between 100 and 10000.");
+            }
+
+            if (workRate < 0.25m || workRate > 1.5m)
+            {
+                throw new ArgumentException($"Invalid value, the {nameof(workRate)} value must be between 0.25 and 1.5.");
+            }
+
+            if (gender != 'M' && gender != 'F')
+            {
+                throw new ArgumentException($"Invalid value, the value of the {nameof(gender)} variable must be M or F.");
+            }
         }
     }
 }
