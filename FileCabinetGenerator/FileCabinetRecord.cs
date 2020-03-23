760c9c92b981444d96deefe3f5e1739c2e95769a
Pavel Boltromyuk <pavel.boltromyuk99@gmail.com>
2020-03-24 00:41:12 +0300

    Implement records generator.

---
+++ FileCabinetGenerator/FileCabinetRecord.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace FileCabinetGenerator
+{
+    /// <summary>This class describes the record entity.</summary>
+    public class FileCabinetRecord
+    {
+        /// <summary>
+        /// Gets or sets id.
+        /// </summary>
+        /// <value>
+        /// Integer number.
+        /// </value>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets FirstName.
+        /// </summary>
+        /// <value>
+        /// FirstName.
+        /// </value>
+        public string FirstName { get; set; }
+
+        /// <summary>
+        /// Gets or sets LastName.
+        /// </summary>
+        /// <value>
+        /// LastName.
+        /// </value>
+        public string LastName { get; set; }
+
+        /// <summary>
+        /// Gets or sets DateOfBirth.
+        /// </summary>
+        /// <value>
+        /// DateOfBirth.
+        /// </value>
+        public DateTime DateOfBirth { get; set; }
+
+        /// <summary>
+        /// Gets or sets Salary.
+        /// </summary>
+        /// <value>
+        /// Salary.
+        /// </value>
+        public short Salary { get; set; }
+
+        /// <summary>
+        /// Gets or sets WorkRate.
+        /// </summary>
+        /// <value>
+        /// WorkRate.
+        /// </value>
+        public decimal WorkRate { get; set; }
+
+        /// <summary>
+        /// Gets or sets Gender.
+        /// </summary>
+        /// <value>
+        /// Gender.
+        /// </value>
+        public char Gender { get; set; }
+
+        /// <summary>Converts to string.</summary>
+        /// <returns>A <see cref="string"/> that represents this instance.</returns>
+        public override string ToString()
+        {
+            string result = $"{this.Id}, {this.FirstName}, {this.LastName}, {this.DateOfBirth.ToString("yyyy-MMM-dd", CultureInfo.InvariantCulture)}," +
+                $" {this.Salary}, {this.WorkRate}, {this.Gender}";
+
+            return result;
+        }
+    }
+}
--- FileCabinetGenerator/Program.cs
+++ FileCabinetGenerator/Program.cs
@@ -4,14 +4,17 @@ using System.Linq;
 
 namespace FileCabinetGenerator
 {
-    class Program
+    /// <summary>Class Program.</summary>
+    public static class Program
     {
         private static string formatType;
         private static string fileName;
         private static int amountRecords;
         private static int startId;
 
-        static void Main(string[] args)
+        /// <summary>Defines the entry point of the application.</summary>
+        /// <param name="args">The arguments.</param>
+        public static void Main(string[] args)
         {
             ParseCommandLine(args);
             Console.WriteLine($"{amountRecords} records were written to {fileName}");
@@ -42,15 +45,45 @@ namespace FileCabinetGenerator
 
             formatType = arguments[0];
             fileName = arguments[1];
-            int.TryParse(arguments[2], out amountRecords);
-            int.TryParse(arguments[3], out startId);
+            _ = int.TryParse(arguments[2], out amountRecords);
+            _ = int.TryParse(arguments[3], out startId);
         }
 
         private static bool IsFullParameter(string[] args)
         {
-            bool result = args.Any(x => x.Contains("--"));
+            bool result = args.Any(x => x.Contains("--", StringComparison.Ordinal));
 
             return result;
+        }
+
+        private static List<FileCabinetRecord> Generator(int startId, int recordAmoutn)
+        {
+            var recordsList = new List<FileCabinetRecord>(recordAmoutn);
+            Random random = new Random();
+
+            string genderChars = "MF";
+            decimal[] workRates = new decimal[] { 0.25m, 0.5m, 0.75m, 1.0m, 1.25m, 1.5m };
+            DateTime minDate = new DateTime(1950, 1, 1);
+            DateTime maxDate = DateTime.Now;
+            int days = Convert.ToInt32(maxDate.Subtract(minDate).TotalDays + 1);
+
+            for (int i = 0; i < recordAmoutn; i++)
+            {
+                FileCabinetRecord record = new FileCabinetRecord()
+                {
+                    Id = startId++,
+                    FirstName = "FirstName" + random.Next(0, 99999),
+                    LastName = "LastName" + random.Next(0, 99999),
+                    DateOfBirth = minDate.AddDays(random.Next(0, days)),
+                    Salary = (short)random.Next(100, 10001),
+                    WorkRate = workRates[random.Next(0, workRates.Length)],
+                    Gender = genderChars[random.Next(0, genderChars.Length)],
+                };
+
+                recordsList.Add(record);
+            }
+
+            return recordsList;
         }
     }
 }
