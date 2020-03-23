fc4a91e9663d455b2df1876468235a2f41c0b976
Pavel Boltromyuk <pavel.boltromyuk99@gmail.com>
2020-03-23 23:48:34 +0300

    Added support for command line options.

--- FileCabinetGenerator/Program.cs
+++ FileCabinetGenerator/Program.cs
@@ -1,12 +1,56 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace FileCabinetGenerator
 {
     class Program
     {
+        private static string formatType;
+        private static string fileName;
+        private static int amountRecords;
+        private static int startId;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ParseCommandLine(args);
+            Console.WriteLine($"{amountRecords} records were written to {fileName}");
+        }
+
+        private static void ParseCommandLine(string[] args)
+        {
+            if (args is null)
+            {
+                throw new ArgumentNullException($"{nameof(args)} can't be null.");
+            }
+
+            if (args.Length < 4)
+            {
+                throw new ArgumentException($"{nameof(args)} length can't be less 1.");
+            }
+
+            List<string> arguments;
+
+            if (IsFullParameter(args))
+            {
+                arguments = args.Select(x => x.Split("=")[1]).ToList();
+            }
+            else
+            {
+                arguments = Enumerable.Range(0, args.Length).Where(x => x % 2 != 0).Select(x => args[x]).ToList();
+            }
+
+            formatType = arguments[0];
+            fileName = arguments[1];
+            int.TryParse(arguments[2], out amountRecords);
+            int.TryParse(arguments[3], out startId);
+        }
+
+        private static bool IsFullParameter(string[] args)
+        {
+            bool result = args.Any(x => x.Contains("--"));
+
+            return result;
         }
     }
 }
