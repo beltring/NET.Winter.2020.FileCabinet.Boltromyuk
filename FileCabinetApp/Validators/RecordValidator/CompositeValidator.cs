36b9c2b54c6fd166965413fa39d096a5f7c12e27
Pavel Boltromyuk <pavel.boltromyuk99@gmail.com>
2020-04-05 00:28:00 +0300

    ValidatorBuilder has been added instead of DefaultValidator and CustomValidator.

--- FileCabinetApp/Validators/RecordValidator/CompositeValidator.cs
+++ FileCabinetApp/Validators/RecordValidator/CompositeValidator.cs
@@ -16,7 +16,7 @@ namespace FileCabinetApp.Validators.RecordValidator
         /// Initializes a new instance of the <see cref="CompositeValidator"/> class.
         /// </summary>
         /// <param name="validators">The validators.</param>
-        protected CompositeValidator(IEnumerable<IRecordValidator> validators)
+        public CompositeValidator(IEnumerable<IRecordValidator> validators)
         {
             this.validators = validators.ToList();
         }
---
+++ FileCabinetApp/Validators/RecordValidator/ValidatorBuilder.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FileCabinetApp.Interfaces;
+
+namespace FileCabinetApp.Validators.RecordValidator
+{
+    /// <summary>
+    /// Validator builder.
+    /// </summary>
+    public class ValidatorBuilder
+    {
+        private readonly List<IRecordValidator> validators;
+
+        /// <summary>Initializes a new instance of the <see cref="ValidatorBuilder"/> class.</summary>
+        /// <param name="validators">The validators.</param>
+        public ValidatorBuilder(List<IRecordValidator> validators)
+        {
+            this.validators = validators;
+        }
+
+        /// <summary>Creates this instance.</summary>
+        /// <returns>Validator.</returns>
+        public IRecordValidator Create() => new CompositeValidator(this.validators);
+
+        /// <summary>Validates the first name.</summary>
+        /// <param name="minLength">The minimum length.</param>
+        /// <param name="maxLength">The maximum length.</param>
+        public void ValidateFirstName(int minLength, int maxLength)
+        {
+            this.validators.Add(new FirstNameValidator(minLength, maxLength));
+        }
+
+        /// <summary>Validates the last name.</summary>
+        /// <param name="minLength">The minimum length.</param>
+        /// <param name="maxLength">The maximum length.</param>
+        public void ValidateLastName(int minLength, int maxLength)
+        {
+            this.validators.Add(new LastNameValidator(minLength, maxLength));
+        }
+
+        /// <summary>Validates the date of birth.</summary>
+        /// <param name="from">From.</param>
+        /// <param name="to">To.</param>
+        public void ValidateDateOfBirth(DateTime from, DateTime to)
+        {
+            this.validators.Add(new DateOfBirthValidator(from, to));
+        }
+
+        /// <summary>Validates the salary.</summary>
+        /// <param name="min">The minimum.</param>
+        /// <param name="max">The maximum.</param>
+        public void ValidateSalary(short min, short max)
+        {
+            this.validators.Add(new SalaryValidator(min, max));
+        }
+
+        /// <summary>Validates the work rate.</summary>
+        /// <param name="min">The minimum.</param>
+        /// <param name="max">The maximum.</param>
+        public void ValidateWorkRate(decimal min, decimal max)
+        {
+            this.validators.Add(new WorkRateValidator(min, max));
+        }
+
+        /// <summary>Validates the gender.</summary>
+        /// <param name="gender">The gender.</param>
+        public void ValidateGender(char[] gender)
+        {
+            this.validators.Add(new GenderValidator(gender));
+        }
+    }
+}
